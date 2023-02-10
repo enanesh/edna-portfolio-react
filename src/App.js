202fbc84cc4d8bac7d4f63e66d395f63af779c90
enanesh <edna.ganesha.gr@gmail.com>
2023-02-09 23:41:03 -0800

    Changes added to a side bar, it's showing the icons rn is reponsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
 
 
 function App() {
 
   return (
     <div>
-    <NavBar />
+      <NavBar />
+      <Home />
+
+      
+      <SocialLinks />
 
     </div>
   );
