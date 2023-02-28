0202fee80cbee6dbf64d33cced620c9448a207de
enanesh <edna.ganesha.gr@gmail.com>
2023-02-28 00:12:03 -0800

    Changes added to a Portfolio comopnent, adds the images i want to use to create the snippets from my projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
+import installNode from '../assets/portfolio/installNode.jpg'
+import navbar from '../assets/portfolio/navbar.jpg'
+import usestate from '../assets/portfolio/usestate.jpg'
+import project1 from '../assets/portfolio/project1.jpg'
+import project2 from '../assets/portfolio/project2.jpg'
 
 const Portfolio = () => {
   return (
