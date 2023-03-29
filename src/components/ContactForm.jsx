08b4fabf56d5bf289f0a3f024729c794972599cc
enanesh <edna.ganesha.gr@gmail.com>
2023-03-29 13:41:43 -0700

    Adds end-point to form component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,58 +1,7 @@
-// Here we import a helper function that will check if the email is valid
-import { validateEmail } from "../utils/helpers";
-import { useState, useRef, useEffect } from "react";
+
 
 function ContactForm() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState("");
-  const [userName, setUserName] = useState("");
-  const [message, setMessage] = useState("");
-  const [showAlert, setShowAlert] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-  const inputRef = useRef(null);
-
-  //Handles the event when the mouse is out of the textbox
-
-  const handleBlur = (event) => {
-    if (!message) {
-      setShowAlert(true);
-    }
-    if (inputRef.current && !inputRef.current.contains(event.target)) {
-      setShowAlert(false);
-    }
-  };
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === "email") {
-      setEmail(inputValue);
-    } else if (inputType === "userName") {
-      setUserName(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage("Enter a valid email e.g: email@email.com");
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-    }
-    if (!message)
-      // If everything goes according to plan, we want to clear out the input after a successful registration.
-      setUserName("");
-    setEmail("");
-    setMessage("");
-  };
+  
 
   return (
     <div
@@ -62,24 +11,23 @@ function ContactForm() {
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full ">
         <div className="pb-8">
           <p className="text-4xl text-white font-bold inline border-b-4 border-purple-500">
-            Contact{" "}
+            Contact
           </p>
           <p className="py-4 text-xl">
-            {" "}
             Submit the form below to get in touch with me.
           </p>
         </div>
 
         <div className="flex justify-center items-center">
+
           <form
-            action="https://getform.io/f/6d94ecd1-b25a-46a8-9368-7a8fb8c6b31e"
+            action="https://getform.io/f/379bf679-72e2-4135-af3c-1d16899c6d8e"
+            method="POST"
             className="flex flex-col w-full md:1/2"
           >
             <p className="text-xl">Name:</p>
             <input
-              value={userName}
-              name="userName"
-              onChange={handleInputChange}
+              name="username"
               type="text"
               placeholder="Type your name"
               className="p-2 bg-transparent border-2 rounded-md text-white focus-outline-none"
@@ -88,46 +36,26 @@ function ContactForm() {
             <p className="pt-2 text-xl">Email :</p>
 
             <input
-              value={email}
               name="email"
-              onChange={handleInputChange}
               type="email"
               placeholder="Type your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus-outline-none"
             />
-            {errorMessage && (
-              <div>
-                <p className="error-text py-2 bg-pink-500 rounded">
-                  {errorMessage}
-                </p>
-              </div>
-            )}
 
             <p className="text-xl">Message: </p>
             <textarea
-              value={message}
               name="message"
               rows="10"
               placeholder="Type your message"
-              onChange={(e) => setMessage(e.target.value)}
-              onMouseOut={handleBlur}
-              ref={inputRef}
               className="p-2 bg-transparent border-2 rounded-md text-white focus-outline-none"
             />
-            
-            {showAlert && (
-              <div className="py-2 bg-pink-500 rounded">
-                Please enter a value!
-              </div>
-            )}
 
             <button
-              type="button"
-              onClick={handleFormSubmit}
               className="text-white w-fit px-6 py-3 my-2 flex items-center  rounded-md bg-gradient-to-r from-purple-800 to-pink-500"
             >
               Submit
             </button>
+
           </form>
         </div>
       </div>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -39,7 +39,11 @@ const Navigation = () => {
     return (
       <div className="flex justify-between items-center w-full h20 px-6 bg-gradient-to-r from-black to-purple-800 text-white fixed">
         <div>
-          <h1 className="text-5xl font-signature py-6 ml-2">Edna Gonzalez </h1>
+          <Link to={"home"}>
+            <h1 className="text-5xl font-signature py-6 ml-2">
+              Edna Gonzalez{" "}
+            </h1>
+          </Link>
         </div>
         {/* maps the array  to the ul list  */}
         <ul className="hidden md:flex">
@@ -48,15 +52,10 @@ const Navigation = () => {
               key={id}
               className="px-4 cursor-pointer- text-lg hover:bg-pink-500 rounded-md p-4 text-white  capitalize hover:scale-105 duration-200"
             >
-              
-                  <Link
-                      to={link}
-                      smooth duration={500}>
+              <Link to={link} smooth duration={500}>
                 {link}
-                  </Link>
-                  
-              </li>
-              
+              </Link>
+            </li>
           ))}
         </ul>
 
